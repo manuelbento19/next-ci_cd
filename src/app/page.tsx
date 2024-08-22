f626e54c4857f5fede033a50a5d74f404d9b4b42
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-22 10:08:03 +0100

    feat: Husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CI/CD with Next.js + Vitest + Husky + Github Actions</h1>
+      <h1>CI/CD with Next.js + Vitest + Husky + Lint Staged + Github Actions</h1>
     </main>
   );
 }
