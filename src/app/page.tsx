bda9de1718c147652821bd165ef47e3f3c2a9d3b
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-22 09:38:49 +0100

    feat: Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>CI/CD with Next.js + Vitest + Husky + Github Actions</h1>
+    </main>
+  );
+}
